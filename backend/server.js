4a939d0189d0ccdc4f23d6258683bc08f50c43fc
Riya Varia <rvaria@bayviewglen.ca>
2026-02-13 14:27:45 -0500

    allow CORS from all origins

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 
